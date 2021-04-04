3475867f7d8cff9b0d0c8b1d495105aade9ca322
Emily <yeouvi29@gmail.com>
2021-04-03 23:42:15 -0700

    edit async functions on server side delete  endpoint array add senddata to client side

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,3 @@
-import { functionsIn } from "lodash";
-
 const { default: fetch } = require("node-fetch");
 
 
@@ -9,26 +7,23 @@ function performAction(event) {
    
     const cityName = document.getElementById("city-input").value;
     let departureDate = document.getElementById("date").value;
+
     console.log("it's clicked")
     // if (Client.checkForZip(zipText)) {
     //     alert("I can't find your zip code.\n Please enter your zip code again.");
     // } else {
     // (function (data) {
    
-        postWeather("/addGeo", 
+        postWeather( 
         { city:cityName, date: departureDate })
-    // })      
-           
-     
-        
-    // }
+        updateUI()
 };
 
 
 /* POST */
-const postWeather = async (url="", data = {} )=>{
+const postWeather = async (data = {}) => {
     console.log("hello")
-    const response = await fetch(url, {
+    const response = await fetch("/addGeo", {
         method: 'POST', 
         credentials: 'same-origin', 
         headers: {
@@ -48,65 +43,17 @@ const postWeather = async (url="", data = {} )=>{
     }
 }
 
-/* Update UI */
+// /* Update UI */
+const updateUI = async (url = "") => {
+    const response = await fetch(url);
+    console.log(response.body)
 
-async function updateUI(res) {
-        console.log(res);
-
-        let departureDate = document.getElementById("date").value;
-        document.getElementById("d-day").innerHTML = departureDate;
-        
-        // get the selectors by Id, and update property dynamically
-        document.getElementById("days").innerHTML = dayCount(departureDate);
-        console.log(dayCount(departureDate));
-        document.getElementById("city").innerHTML = res.geonames[0].name; //city
-        document.getElementById("country").innerHTML = res.geonames[0].countryName; //country
-        document.getElementById("latitude").innerHTML = res.geonames[0].lat; //latitude
-        document.getElementById("longitude").innerHTML = res.geonames[0].lng; //longitude
-        document.getElementById("weather").classList.add("#weather");      
-}
-
-/*count down*/
-const dayCount = function(departureDate) {
-    // const departureDate = document.getElementById("date").value;
-    console.log("departuredate", departureDate);
-    const month = departureDate.slice(0,2);
-    console.log(month);
-    const day = departureDate.slice(3,5);
-    console.log(day);
-    const year = departureDate.slice(6,10);
-    console.log(year);
-    const theDate = new Date(year, month - 1, day);
-    console.log(theDate);
-    const today = new Date();
-    console.log("big",theDate - today);
-
-    if (theDate >= today) {
-        return (Math.round((theDate - today) / (1000 * 60 * 60 * 24 ))+1);
-    } else {
-        alert ("Please enter future date.")
+    try {
+        const dataLocation = request.json();
+        console.log(dataLocation);
+    } catch(error) {
+        console.log("error", error);
     }
 }
-
-/* Check leapyear */
-const leapYear = function(yearNow) {
-    let totalDate;
-    if (yearNow % 4 === 0) {
-        if (yearNow % 100 === 0) {
-            if (yearNow % 400 === 0) {
-                 return true;
-            } else {
-                return false;
-            }
-        } else {
-            totalDate === 366;
-        }
-    } else {
-        totalDate === 365;
-    }
-}
-
-
 
 export {performAction}
-export {updateUI}
--- src/server/server.js
+++ src/server/server.js
@@ -8,8 +8,6 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const axios = require("axios")
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('dist'))
@@ -26,20 +24,12 @@ app.get("/", res => {
     res.sendFile(path.resolve("dist/index.html"))
 })
 
-
-
-
 app.post("/addGeo", async (req, res) => {
     getGeoInfo(req)
-    .then(
-        function(data) {
-            getWeatherInfo(data)
-        }
-        
-    )
+    .then(data => getWeatherInfo(data))
+    .then(weatherData => getPhoto(weatherData))
+    .then(weatherData => res.send(weatherData))
 })
-
-const weatherData = [];
 
 const getGeoInfo = async (req) => {
     const geoBaseURL = 'http://api.geonames.org/searchJSON?q='
@@ -47,12 +37,10 @@ const getGeoInfo = async (req) => {
     const maxRows = '&maxRows=10&username='
 
     try {
-        console.log(req.body)
         const response =  await axios({
         method: "post",
         url: geoBaseURL + req.body.city + maxRows + geoAPI
         })
-        console.log(response);
         const geoData = {
             city: req.body.city,
             date: req.body.date,
@@ -61,75 +49,69 @@ const getGeoInfo = async (req) => {
             latitude: response.data.geonames[0].lat,
             longitude: response.data.geonames[0].lng
         }
-        console.log("geodata",geoData);
-        weatherData.push(geoData);
-        return weatherData;
-
-        
+        return geoData;      
     } catch (err) {
         console.log("error", err)
     }   
-
 }
 
-const getWeatherInfo = async (res) => {
-    console.log(res);
-    const weaBaseURL = 'https://api.weatherbit.io/v2.0/normals?key='
-    const weaAPI = process.env.WEATHER_KEY
-    const day = res[0].date.slice(0,2);
-    const nextDay = "0" + (parseInt(day) + 1).toString()
-    const month = res[0].date.slice(3,5);
-    const year = res[0].date.slice(6,10);
+const getWeatherInfo = async (geoData) => {
+    console.log(geoData);
+    const weaBaseURL = 'https://api.weatherbit.io/v2.0/normals?key=';
+    const weaAPI = process.env.WEATHER_KEY;
+    const day = geoData.date.slice(0,2);
+    const nextDay = "0" + (parseInt(day) + 1).toString();
+    const month = geoData.date.slice(3,5);
+    const year = geoData.date.slice(6,10);
     const changeDateFormat = month + "-" + day;
-    const setNextDay = month + "-" + nextDay
-    console.log(changeDateFormat);
-    const lat = "&lat=" + (Math.round(res[0].latitude * 100) / 100);
-    const lon = "&lon=" + (Math.round(res[0].longitude* 100) / 100);
-    const date = "&start_day=" + changeDateFormat + "&end_day=" + setNextDay + "&units=I"
+    const setNextDay = month + "-" + nextDay;
+    const lat = "&lat=" + (Math.round(geoData.latitude * 100) / 100);
+    const lon = "&lon=" + (Math.round(geoData.longitude* 100) / 100);
+    const date = "&start_day=" + changeDateFormat + "&end_day=" + setNextDay + "&units=I";
+
+    try {
+        const response = await axios({
+            method: "post",
+            url: weaBaseURL + weaAPI + lat + lon + date
+        })
+        const temperature = {
+            tempHigh: response.data.data[0].max_temp,
+            tempLow: response.data.data[0].min_temp
+        }
+        let weatherData = {
+            geoData: geoData,
+            temperature: temperature
+        }
+        // console.log("weatherData", weatherData)
+        return weatherData;        
+    } catch (err) {
+        console.log("error", err)
+    }   
+}
+
+const getPhoto = async (weatherData) => {
+    console.log("real", weatherData);
+    const pixaBayURL = 'https://pixabay.com/api/?key=';
+    const photoAPI = process.env.PIXABAY_KEY;
+    const q = "&q=" + encodeURIComponent(weatherData.geoData.city);
+    const otherSetting = "&editors_choice=true&image_type=photo"
 
     try {
         const response =  await axios({
         method: "post",
-        url: weaBaseURL + weaAPI + lat + lon + date
+        url: pixaBayURL + photoAPI + q + otherSetting
         })
-        console.log("weatherbit",response);
-        const weaData = {
-            tempHigh: response.data.data[0].max_temp,
-            tempLow: response.data.data[0].min_temp
+        const photoData = {photo: ""};
+        if (response.data.total === 0) {
+            photoData.photo = "https://pixabay.com/photos/beach-beautiful-beetle-classic-car-1853939/"
+        } else {
+            photoData.photo = response.data.hits[0].webformatURL
         }
-        weatherData.push(weaData)
-        weatherData.push(res)
+        // console.log("response", response);
+        weatherData.photoData = photoData
         console.log("weatherData", weatherData)
-        return weatherData;
-
-        
+        return weatherData;       
     } catch (err) {
         console.log("error", err)
     }   
 }
-
-
-// const getPhoto = async (res) => {
-//     const pixaBayURL = 'https://pixabay.com/api/?key=';
-//     const weaAPI = process.env.PIXABAY_KEY;
-    
-
-//     try {
-//         const response =  await axios({
-//         method: "post",
-//         url: weaBaseURL + weaAPI + lat + lon + date
-//         })
-//         console.log("weatherbit",response);
-//         const weaData = {
-//             tempHigh: response.data.data[0].max_temp,
-//             tempLow: response.data.data[0].min_temp
-//         }
-//         weatherData.push(weaData)
-//         // weatherData.push(res)
-//         console.log("weatherData", weatherData)
-
-        
-//     } catch (err) {
-//         console.log("error", err)
-//     }   
-// }
